e15376d30d95787223ee34b1fa2051d4a9369c09
kimmanlui <cskmlui@hotmail.com>
2026-02-23 14:25:58 +0800

    Update calc_cond_prob.r

--- calc_cond_prob.r
+++ calc_cond_prob.r
@@ -112,8 +112,9 @@ fixColonFormat=function(df, col_name='Weekday')
 #' df<-data.frame(exam_lang_score=c(80,88,85,82,34,34),age=c(6,7,8,6,7,8),height =c(5,6,6,7,5,7))
 #' ## Find P(exam_lang_score >= 80 ~ age + height) where age is divided into 3 groups and height into 4 groups.   
 #' res=calc_cond_prob(df, "exam_lang_score >= 80 ~ age + height", range_list=list( 3,4))
-#' ## Extract the results of the high and low odds obtained from executing the cal_cond_prob() function.         
+#' ## Use the results to calculate the conditional probability for the power set of 'age' and 'height'.         
 #' summary_result_list=shortSummary(res[[1]], "age + height ", combination=1)
+#' ## Extract the results of the high and low odds obtained from executing the cal_cond_prob() function. 
 #' lapply(summary_result_list, goodchance, upper=0.7, lower=0.25)
 goodchance=function(df, col_name='odd', upper=0.75, lower=0.25)
 {
