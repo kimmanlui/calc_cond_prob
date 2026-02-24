2534a692d7aa7c0f7eac144e18a684e3ce820583
kimmanlui <cskmlui@hotmail.com>
2026-02-24 12:26:17 +0800

    Update calc_cond_prob.r

--- calc_cond_prob.r
+++ calc_cond_prob.r
@@ -1,14 +1,18 @@
-# Copyright 2016
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
+###############################################################################
+# rangecondprob:  Tool for Conditional Probability                            #
+# Copyright (C) 2026 Kim Man Lui                                              #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU General Public License as published by        #
+# the Free Software Foundation, either version 3 of the License, or           #
+# any later version.                                                          #
+#                                                                             #
+# This program is distributed in the hope that it will be useful, but         #
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License    #
+# for more details: <https://www.gnu.org/licenses/>.                          #
+###############################################################################
+
 
 
 library(dplyr)
@@ -367,3 +371,4 @@ calc_cond_prob=function(clean_data, formula_string=NULL, range_list, cond_evalua
     goodchanceRes=goodchance(res)
     return(list( results=res,  goodchance=goodchanceRes, range_list=range_list))
 }
+
